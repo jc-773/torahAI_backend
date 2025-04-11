68f6c3d5ffd75448a39bb08fcd70a4084556aab4
jc-773 <jonc960@gmail.com>
2025-04-11 09:28:32 -0500

    init commit

---
+++ src/main/java/com/torah/torahAI/TorahAiApplication.java
@@ -0,0 +1,13 @@
+package com.torah.torahAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TorahAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TorahAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/torah/torahAI/TorahAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.torah.torahAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TorahAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
